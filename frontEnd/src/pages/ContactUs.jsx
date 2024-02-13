ae787f6d7d95d50449e1adb7d153a605f0eb1be8
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2024-02-13 18:00:30 +0530

    learn about browserRouter

---
+++ frontEnd/src/pages/ContactUs.jsx
@@ -0,0 +1,9 @@
+function ContactUs(){
+
+    return (
+        <>
+        <h1>This is contact us page</h1>
+        </>
+    )
+}
+export default ContactUs
---
+++ frontEnd/src/pages/Initiative.jsx
@@ -0,0 +1,8 @@
+function Initiative(){
+    return(
+        <>
+        <h1>This is a short initiative of our for the community.</h1>       
+        </>
+    )
+}
+export default Initiative;
