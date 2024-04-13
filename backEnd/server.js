d02df1c11574ec124595ec7a2cbc3f18bbdafab7
chandu <cmhandan@gmail.com>
2024-04-13 19:57:32 +0530

    added getVegetable enpoint

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const fs = require('fs');
+const port = 3500;
+
+app.get("/api/intro", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get('/api/summerVegetable',(req,res)=>{
+    fs.readFile('SummerPlants.json','utf-8' , (err,data)=>{
+        if(err) res.send('Internal server error');
+        data = JSON.parse(data);
+        res.send(data);
+    })
+})
 
 app.get('/',(req,res)=>{
-res.send('Hello World')
+  res.send('hello you did hit this endpoint');
 })
 
+
 app.listen(port, () => {
-    
-console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
