0ad89d17c0139214ce3b65c588e5ee883f619582
chandu <cmhandan@gmail.com>
2024-04-22 09:00:17 +0530

    updated

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const bodyParser = require("body-parser");
-const port = 3500;
+const port = 3000;
 app.use(bodyParser.json());
 
 app.get("/api/summerVegetable", (req, res) => {
